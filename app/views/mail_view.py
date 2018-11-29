d895329c7badc062b864e6ff97dec8756333a868
fionn <fxujunfeng@foxmail.com>
2018-11-29 14:02:00 +0800

    去除主线程中的Mail实例化以及初始化

--- app/views/mail_view.py
+++ app/views/mail_view.py
@@ -4,12 +4,13 @@ from utils.email import send_async_mail
 
 blue = Blueprint('mailbp',__name__)
 
-@blue.route("/mail")
+@blue.route("/email/")
 def sendmail():
     subject = "Hello"
     to_addr = 'fxujunfeng@foxmail.com'
     body = "Hello Flask"
     html = "<h1>This is a mail test!<h1>"
+
     send_async_mail(subject,to_addr,body,html)
 
     return 'ok'
--- utils/email.py
+++ utils/email.py
@@ -1,5 +1,5 @@
 from threading import Thread
-from flask import  Flask
+from flask import Flask
 from flask_mail import Message, Mail
 
 '''
@@ -7,24 +7,42 @@ from flask_mail import Message, Mail
     规避了应用程序上下文错乱的问题和循环引用的问题
 '''
 
-f_app = Flask('mail')
+f_app = Flask('email')
+
 f_app.config.update(
     # 邮箱基础配置
     MAIL_SERVER = "smtp.qq.com",
     MAIL_PORT = 465,
     MAIL_USE_SSL = True,
-    MAIL_USERNAME = 'xxxx@qq.com',
-    MAIL_PASSWORD = 'xxxx',
-    MAIL_DEFAULT_SENDER = 'xxxxx@qq.com'
+    MAIL_USERNAME = '2809276444@qq.com',
+    MAIL_PASSWORD = 'udfumdkgeqledeja',      #邮箱授码
+    MAIL_DEFAULT_SENDER = '2809276444@qq.com',
 )
 
-mail = Mail(f_app)
+email = Mail(f_app)
 
 # send email asynchronously
-def _send_async_mail(app, message):
+#注意，由于主线程中没有对flask_main插件的Mail对象进行实例化和初始化，所以Message依赖的属性没有初始化，所以无法在主线程中进行实例化
+#解决方法：
+#   子线程对Mail进行了实例化和初始化，所以Message在子线程中实例化
+
+def _send_async_mail(app, mess):
 
     with app.app_context():
-        mail.send(message)
+        message = Message(mess['subject'])
+        if isinstance(mess['to'], list):
+            message.recipients = mess['to']
+        else:
+            message.recipients = [mess['to']]
+
+        if mess['body']:
+            message.body = mess['body']
+
+        if mess['html']:
+            message.html = mess['html']
+
+        email.send(message)
+
 
 
 def send_async_mail(subject, to,body=None,html=None):
@@ -38,17 +56,12 @@ def send_async_mail(subject, to,body=None,html=None):
     :return: 创建的线程对象
     '''
 
-    message = Message(subject)
-    if isinstance(to, list):
-        message.recipients = to
-    else:
-        message.recipients = [to]
-
-    if body:
-        message.body = body
-
-    if html :
-        message.html = html
+    message = {
+        'subject':subject,
+        'to':to,
+        'body':body,
+        'html':html,
+    }
 
     # 通过线程实现异步调用
     thr = Thread(target=_send_async_mail, args=[f_app, message])
